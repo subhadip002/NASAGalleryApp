b1ca4f0755b9f28af11f15a4b0f6e505b8dd2396
subhadipnath <subhadipnath2011@gmail.com>
2022-10-04 13:11:53 +0530

    Shimmer implemented

--- app/src/main/java/com/example/nasagalleryapp/util/BindingAdapters.kt
+++ app/src/main/java/com/example/nasagalleryapp/util/BindingAdapters.kt
@@ -1,5 +1,6 @@
 package com.example.nasagalleryapp.util
 
+import android.view.View
 import android.widget.ImageView
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.databinding.BindingAdapter
@@ -8,6 +9,7 @@ import coil.load
 import com.example.nasagalleryapp.R
 import com.example.nasagalleryapp.ui.ImageGridAdapter
 import com.example.nasagalleryapp.ui.ImageItemUiState
+import com.facebook.shimmer.ShimmerFrameLayout
 
 @BindingAdapter("imageList")
 fun bindRecyclerView(recyclerView: RecyclerView, data: List<ImageItemUiState>) {
@@ -32,3 +34,20 @@ fun loadHDImage(
         }
     }
 }
+
+@BindingAdapter("isVisible")
+fun bindVisibility(view: View, isVisible: Boolean) {
+    view.visibility = if (isVisible) View.VISIBLE else View.GONE
+}
+
+@BindingAdapter("dataSize")
+fun bindShimmer(shimmerFrameLayout: ShimmerFrameLayout, dataSize: Int) {
+    if (dataSize > 0) {
+        shimmerFrameLayout.stopShimmer()
+        bindVisibility(shimmerFrameLayout, false)
+    } else {
+        shimmerFrameLayout.startShimmer()
+        bindVisibility(shimmerFrameLayout, true)
+    }
+}
+
